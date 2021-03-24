aaf3ea36dbd30f110fe4edd0385ea4e887f7a03c
Rolando Alvarez <rolandoalvarezfaye@gmail.com>
2021-03-24 19:01:30 -0400

    validating new data for position

--- lib/board.rb
+++ lib/board.rb
@@ -6,6 +6,16 @@ class Board
     set_grid
   end
 
+  def set_value_grid(position, mark)
+    new_value = position.to_i
+    if valid? && new_value > 1 && new_value <= 9
+      @value = value
+      true
+    else
+      false
+    end
+  end
+
   private
   def default_grid
       Array.new(3) { Array.new(3) { Cell.new }}
@@ -15,5 +25,12 @@ class Board
     if @cell_grid.nil?
       @cell_grid = default_grid
     end
+  end
+
+  def get_x_y (position)
+    x = position/3
+    y = position % 3
+
+    {}
   end
 end
